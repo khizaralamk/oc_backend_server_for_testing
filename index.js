119c289e48ece0805c373761575839de011e8e48
khizaralamk <khizar@alphaventure.com>
2025-11-10 19:03:13 +0500

    vC

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const authRouter = require('./routes/auth');
 const serviceRoute = require('./routes/servicesRoute');
 const fullBodyRoute = require('./routes/fullBodyRoute');  
 const fetchRestaurantItemsRoute = require('./routes/fetchRestaurantItems');
+const cors = require('cors');
+
 
 app.use(express.json());
+
+app.use(cors());
 app.use('/api/hello', helloRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/services', serviceRoute );
